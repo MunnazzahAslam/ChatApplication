941cf3cacbbd1afad5c15061103d973449f1bb6a
Munnazzah Aslam <aslammunnazzah@gmail.com>
2020-08-30 23:50:10 +0500

    Added scroll to bottom effect

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,7 @@
 const express= require('express');
 const socketio= require('socket.io');
 const http= require('http');
-
+const cors= require('cors');
 const {addUser,removeUser,getUser,getUsersInRoom} =require('./users.js');
 
 const router=require('./router');
@@ -42,4 +42,6 @@ io.on('connect',(socket)=>{
 
 
 app.use(router);
+app.use(cors());
+
 server.listen(PORT, ()=> console.log(`Server has started on port ${PORT}`));
