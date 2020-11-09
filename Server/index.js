e547b4d0cf8687a7dac0429906cecd75e3b8cc67
Munnazzah Aslam <aslammunnazzah@gmail.com>
2020-11-09 19:00:54 +0500

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -3,10 +3,8 @@ const socketio= require('socket.io');
 const http= require('http');
 const cors= require('cors');
 const {addUser,removeUser,getUser,getUsersInRoom} =require('./users.js');
-
 const router=require('./router');
 const PORT=process.env.PORT || 5000;
-
 const app=express();
 const server=http.createServer(app);
 const io=socketio(server);
